dcb3ab8e1be1047f9d092e1aaed391bc3ed86e98
joelermantraut <joelermantraut@gmail.com>
2021-08-06 18:27:56 -0300

    Added Aliases

--- .config/scripts/aliases.zsh
+++ .config/scripts/aliases.zsh
@@ -1,1 +1,32 @@
-../../.oh-my-zsh/custom/aliases.zsh
+alias ls='exa -l'
+alias la='exa -la'
+alias lg='exa -la --git-ignore'
+alias ld='exa -l -D'
+alias tree='exa --tree --level=2 -l'
+alias cls='clear && ls'
+
+alias shutdown='shutdown -h now'
+alias suspend='systemctl suspend'
+
+alias wm_class='xprop WM_CLASS'
+alias rm='trash'
+
+alias config='/usr/bin/git --git-dir=/home/joel/.cfg/ --work-tree=/home/joel'
+alias libs='/usr/bin/git --git-dir=/home/datos/Documentos/Atollic_documents/.libraries/ --work-tree=/home/datos/Documentos/Atollic_documents/'
+alias winconfig='/usr/bin/git --git-dir=/home/windows/.cfg --work-tree=/home/windows'
+
+alias passi='pass insert -e'
+
+alias el='krusader --left .'
+alias er='krusader --right .'
+
+alias addmic='pacmd load-module module-alsa-source device=hw:Loopback,1,0'
+
+alias t='trans en:es'
+alias T='trans es:en'
+
+alias ps='procs --tree'
+
+alias v='nvim'
+
+alias feh='feh --draw-filename --scale-down --borderless --action "echo %n;"'
