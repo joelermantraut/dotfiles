67fe22fcd1ee4417e42aabdbc4d0521e5c5fab21
joelermantraut <joelermantraut@gmail.com>
2021-09-28 16:22:38 -0300

    Minor Changes

--- .config/i3-scripts/autostart.sh
+++ .config/i3-scripts/autostart.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-$HOME/.config/polybar/launch.sh --forest &
-betterlockscreen -u ~/Imagenes/slideshow && betterlockscreen -w &
+$HOME/.config/polybar/forest/launch.sh &
 udiskie -t -a &
 dunst &
+clipster -d &
+redshift &
+~/.screenlayout/i3-screen-layout.sh &
+betterlockscreen -u ~/Imagenes/slideshow && betterlockscreen -w &
+#betterlockscreen -u ~/Imagenes/slideshow &&
+    #feh --bg-fill ~/.cache/betterlockscreen/1-HDMI-0/resize.png ~/.cache/betterlockscreen/1-HDMI-0/resize.png &
