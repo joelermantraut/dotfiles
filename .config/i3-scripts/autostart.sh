36ac878653402d5fbb1b085ddb6283b9185d2bb0
joelermantraut <joelermantraut@gmail.com>
2021-12-20 05:21:42 -0300

    Minor changes

--- .config/i3-scripts/autostart.sh
+++ .config/i3-scripts/autostart.sh
@@ -5,8 +5,5 @@ udiskie -t -a &
 dunst &
 clipster -d &
 redshift &
-#~/.screenlayout/i3-screen-layout.sh &
-~/.screenlayout/screen-layout-1.sh &
+~/.screenlayout/i3-screen-layout.sh &
 betterlockscreen -u ~/Imagenes/slideshow && betterlockscreen -w &
-#betterlockscreen -u ~/Imagenes/slideshow &&
-    #feh --bg-fill ~/.cache/betterlockscreen/1-HDMI-0/resize.png ~/.cache/betterlockscreen/1-HDMI-0/resize.png &
